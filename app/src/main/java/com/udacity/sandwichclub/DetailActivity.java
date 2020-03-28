fdf1ae5ab8414a0984e96da3ee22937d62415c54
mastergammler <raphael-hoppe@gmx.de>
2020-03-28 16:14:17 +0100

    Changed the template to static marginLeft

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,10 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -17,12 +20,16 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private static final int TV_LABEL_CHILD_INDEX = 0;
+    private static final int TV_CONTENT_CHILD_INDEX = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        intiTvLabels();
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -50,7 +57,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -63,7 +70,27 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        setTvContent(R.id.tc_origin,sandwich.getPlaceOfOrigin(),false);
+        setTvContent(R.id.tc_known_as,sandwich.getAlsoKnownAs().toString(),false);
+        setTvContent(R.id.tc_ingredients,sandwich.getIngredients().toString(),false);
+        setTvContent(R.id.tc_description,sandwich.getDescription(),false);
+    }
 
+    private void setTvContent(int parentId,String text,boolean label)
+    {
+        int childIndex = label ? TV_LABEL_CHILD_INDEX : TV_CONTENT_CHILD_INDEX;
+
+        FrameLayout parent = findViewById(parentId);
+        TextView contentView = (TextView) parent.getChildAt(childIndex);
+        contentView.setText(text);
+    }
+
+    private void intiTvLabels()
+    {
+        setTvContent(R.id.tc_description,getResources().getString(R.string.detail_description_label),true);
+        setTvContent(R.id.tc_ingredients,getResources().getString(R.string.detail_ingredients_label),true);
+        setTvContent(R.id.tc_known_as,getResources().getString(R.string.detail_also_known_as_label),true);
+        setTvContent(R.id.tc_origin,getResources().getString(R.string.detail_place_of_origin_label),true);
     }
 }
